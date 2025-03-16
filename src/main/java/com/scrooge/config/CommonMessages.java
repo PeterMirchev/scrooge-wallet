97e55e8458a9165da261d2e93c3b06100f6e8825
Peter <guitaronline89@gmail.com>
2025-03-16 23:08:57 +0200

    HTML and CSS refactoring.

--- src/main/java/com/scrooge/config/CommonMessages.java
+++ src/main/java/com/scrooge/config/CommonMessages.java
@@ -3,8 +3,6 @@ package com.scrooge.config;
 public class CommonMessages {
 
     public static final String WELCOME_MESSAGE = """
-                    Dear %s%n%n
-                    
                     You have registered successfully for Scrooge Wallet.%n%n
                     
                     Best Regards,
